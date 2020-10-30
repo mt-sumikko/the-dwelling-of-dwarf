31c0225006cf305ec39ad6cf39f2cb48bda3ff27
YUIKO SUYAMA <Sumikko-Mountain@users.noreply.github.com>
2020-10-30 19:13:46 +0900

    一旦スライドショーを一切無しにして機能するか再確認します

--- docs/js/index.js
+++ docs/js/index.js
@@ -29,10 +29,10 @@ topButton.onclick = function () {
 
 
 /*---------Swiper.js　スライドショー-------------*/
-
+/*
 window.addEventListener('DOMContentLoaded', function () {
     var swiper06Main = new Swiper('.sample06 .main', {
-        /*loop: true,*/
+
         pagination: '.swiper-pagination',
         paginationClickable: true,
         nextButton: '.swiper-button-next',
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', function () {
     })
 
     var swiper06Thumb = new Swiper('.sample06 .thumbnail', {
-        /*loop: true,*/
+     
         spaceBetween: 10,
         centeredSlides: true,
         slidesPerView: 4,
@@ -49,4 +49,71 @@ window.addEventListener('DOMContentLoaded', function () {
     });
     swiper06Main.params.control = swiper06Thumb;
     swiper06Thumb.params.control = swiper06Main;
-}, false);
+}, false);*/
+
+/*
+$(function () {
+    $('.thumb-item').not('.slick-initialized').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.thumb-item-nav',//サムネイルのクラス名
+        responsive: [{
+            breakpoint: 768,
+            settings: 'unslick'
+        }]
+    });
+    $('.thumb-item-nav').not('.slick-initialized').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.thumb-item', //スライダー本体のクラス名
+        focusOnSelect: true,
+    });
+
+
+    $(window).on('resize orientationchange', function () {
+        $('.thumb-item').slick('resize');
+    });
+});*/
+/*slick.min.js:1 Uncaught TypeError: Cannot read property 'add' of null at Object.e.initADA (slick.min.js:1)
+https://www.proglad.tokyo/2017/08/slickjs-uncaught-typeerror-cannot-read.html */
+
+
+
+/*var swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-my-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + '<img src="https://haniwaman.com/wp-content/uploads/2018/05/swiper-thumbnail' + (index + 1) + '.png" alt="">' + '</span>';
+        },
+    },
+});*/
+
+
+
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    loop: true,
+    freeMode: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    loop: true,
+    loopedSlides: 5, //looped slides should be the same
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+        swiper: galleryThumbs,
+    },
+});
