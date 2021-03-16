46701d4e6947a4ed56c038b1a885f22cfcc743f6
YUIKO SUYAMA <Sumikko-Mountain@users.noreply.github.com>
2021-03-16 23:32:57 +0900

    小人のすみかロゴをこっちのレポジトリに移動した

---
+++ docs/js/main.js
@@ -0,0 +1,21 @@
+document.getElementById("blue").onclick = function () {
+    document.getElementById("color-data").innerHTML = "send blue";
+    console.log("blue");
+};
+
+document.getElementById("skyBlue").onclick = function () {
+    document.getElementById("color-data").innerHTML = "send blue";
+    console.log("skyBlue");
+};
+document.getElementById("green").onclick = function () {
+    document.getElementById("color-data").innerHTML = "send blue";
+    console.log("green");
+};
+document.getElementById("yellow").onclick = function () {
+    document.getElementById("color-data").innerHTML = "send blue";
+    console.log("yellow");
+};
+document.getElementById("red").onclick = function () {
+    document.getElementById("color-data").innerHTML = "send blue";
+    console.log("red");
+};
